65b534aa4e107de6547a94608cfa46a52538a3a9
Glory Wong Zhaohui <glorywong1001@gmail.com>
2021-02-28 20:40:29 +0800

    Initial Commit

---
+++ QuoteScreenSaver/QuoteScreenSaverView.h
@@ -0,0 +1,12 @@
+//
+//  QuoteScreenSaverView.h
+//  QuoteScreenSaver
+//
+//  Created by 王昭辉 on 2021/2/28.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface QuoteScreenSaverView : ScreenSaverView
+
+@end
---
+++ QuoteScreenSaver/QuoteScreenSaverView.m
@@ -0,0 +1,51 @@
+//
+//  QuoteScreenSaverView.m
+//  QuoteScreenSaver
+//
+//  Created by 王昭辉 on 2021/2/28.
+//
+
+#import "QuoteScreenSaverView.h"
+
+@implementation QuoteScreenSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
